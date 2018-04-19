137175361375bcfabb77e645994c170b0d5a7938
iven-yang <risingsky1@gmail.com>
2018-04-19 04:01:45 -0400

    auto queue up/play again added

--- src/LoLAIJgBot.py
+++ src/LoLAIJgBot.py
@@ -28,11 +28,13 @@ images_folder = r'C:\Users\risin\Desktop\DefNotBottin\Python-GUI-Scripts\src\res
 def recall():
     print('recalling')
     MouseClick.left_click(1235, 1245, 1023, 1036)
+    time.sleep(1)
+    MouseClick.left_click(1235, 1245, 1023, 1036)
     time.sleep(10)
 
 def begin_game_shop():
     recall()
-    print('shopping')
+    print('starting items')
     # open shop
     MouseClick.left_click(825, 925, 175, 250)
     time.sleep(random.uniform(0.75, 1.0))
@@ -81,6 +83,8 @@ def shop():
     time.sleep(random.uniform(0.75, 1.0))
 
 def fishlevelups():
+    MouseClick.left_click(950, 960, 940, 955)
+    time.sleep(random.uniform(0.5, 0.75))
     MouseClick.left_click(780, 795, 940, 955)
     time.sleep(random.uniform(0.5, 0.75))
     MouseClick.left_click(840, 860, 940, 955)
@@ -150,7 +154,7 @@ def play():
         recall_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'recall.png'))
         if recall_button is not None:
             print('game start')
-            time.sleep(1)
+            time.sleep(3)
             begin_game_shop()
             break
     else:
@@ -160,7 +164,7 @@ def play():
     timeout = datetime.now() + timedelta(seconds=3600) # 60 min
 
     fishlevelups()
-    redbuff(73)
+    redbuff(70)
     time.sleep(random.uniform(30, 35))
     fishlevelups()
 
@@ -183,20 +187,20 @@ def play():
         if check_post_game():
             break
         
-        wolves(35)
-        time.sleep(random.uniform(30, 35))
+        wolves(31)
+        time.sleep(random.uniform(27, 32))
         fishlevelups()
         if check_post_game():
             break
         
         chickens(25)
-        time.sleep(random.uniform(35, 40))
+        time.sleep(random.uniform(31, 36))
         fishlevelups()
         if check_post_game():
             break
         
         shop()
-        time.sleep(random.uniform(40, 50))
+        time.sleep(random.uniform(15, 20))
         if check_post_game():
             break
         
@@ -213,7 +217,7 @@ def play():
 
 def check_post_game() -> bool:
     skip_honor_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'skip_honor.png'))
-    play_again_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'play_again.png'))
+    play_again_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'x_left_of_play_again.png')) # Using the actual play again button seems inconsistent
     if skip_honor_button is not None:
         time.sleep(1)
         MouseClick.left_click(skip_honor_button[0], skip_honor_button[0], skip_honor_button[1], skip_honor_button[1])
@@ -223,12 +227,20 @@ def check_post_game() -> bool:
     return False
 
 def play_again():
+    # check for level up ok button
+    pyautogui.moveTo(10, 10) # move mouse so level up ok button won't be covered
+    time.sleep(3)
+    level_up_ok_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'level_up_ok.png'))
+    if level_up_ok_button is not None:
+        MouseClick.left_click(level_up_ok_button[0], level_up_ok_button[0], level_up_ok_button[1], level_up_ok_button[1])
+    
+    # click play again
     timeout = datetime.now() + timedelta(seconds=30)
     while timeout > datetime.now():
-        play_again_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'play_again.png'))
+        play_again_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'x_left_of_play_again.png')) # Using the actual play again button seems inconsistent
         if play_again_button is not None:
             time.sleep(1)
-            MouseClick.left_click(play_again_button[0], play_again_button[0], play_again_button[1], play_again_button[1])
+            MouseClick.left_click(play_again_button[0] + 50, play_again_button[0] + 100, play_again_button[1], play_again_button[1])
             time.sleep(5)
             break
     else:
@@ -240,9 +252,9 @@ def find_match():
     
     timeout = datetime.now() + timedelta(seconds=10)
     while timeout > datetime.now():
-        find_match_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'find_match.png'))
+        find_match_button = pyautogui.locateCenterOnScreen(os.path.join(images_folder, 'x_left_of_find_match.png')) # Using the actual find match button seems inconsistent
         if find_match_button is not None:
-            MouseClick.left_click(find_match_button[0], find_match_button[0], find_match_button[1], find_match_button[1])
+            MouseClick.left_click(find_match_button[0] + 50, find_match_button[0] + 100, find_match_button[1], find_match_button[1])
             break
     else:
         print('could not find the find match button')
@@ -256,12 +268,12 @@ def find_match():
             time.sleep(0.5)
             MouseClick.left_click(accept_button[0], accept_button[0], accept_button[1], accept_button[1])
             time.sleep(1)
-            break
+            return accept_button # someone might decline the queue, just keep clicking in same spot
     else:
         print('queue timed out')
         exit(0)
     
-def champ_select():
+def champ_select(accept_button):
     print('champ select')
     # Click Warwick
     timeout = datetime.now() + timedelta(seconds=30)
@@ -285,6 +297,8 @@ def champ_select():
             MouseClick.left_click(lock_in[0], lock_in[0], lock_in[1], lock_in[1])
             time.sleep(10)
             break
+        else:
+            MouseClick.left_click(accept_button[0], accept_button[0], accept_button[1], accept_button[1]) # someone might decline the queue, just keep clicking in same spot
     else:
         print('could not lock in warwick - timeout')
         exit(0)
@@ -294,8 +308,8 @@ try:
     timeout = datetime.now() + timedelta(seconds=36000) # 10 hours
     
     while timeout > datetime.now():
-        find_match()
-        champ_select()
+        accept_button = find_match()
+        champ_select(accept_button)
         play()
         play_again()
 
